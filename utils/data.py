b582652973038ac49b3866ce4e25e015c96b1376
Piotrek Markiewicz <64311472+sectasy0@users.noreply.github.com>
2021-01-22 09:08:30 +0100

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -5,18 +5,18 @@ import asyncio
 
 from typing import Dict
         
-class DataController:
+class DataController(object):
     def __init__(self):
         self.dataFile: str = "data.json"
         self.settingsFile: str = "settings.json"
 
-    async def get_data(self) -> Dict:
+    async def get_data(self) -> Dict[str, any]:
         try:
             with open(self.dataFile, 'r') as f:
                 return json.load(f)
         except (FileNotFoundError, IOError):
             print("\x1b[1;31m[ERROR]:\x1b[0m data.json file not found, create a new.")
-            jsonData: Dict = {
+            jsonData: Dict[str, any] = {
                 "warns": {},
                 "ticket-channel-ids": [],
                 "ticket-id-counter": 0,
@@ -27,13 +27,13 @@ class DataController:
             
             return jsonData
 
-    async def get_settings(self) -> Dict:
+    async def get_settings(self) -> Dict[str, any]:
         try:
             with open(self.settingsFile, 'r') as f:
                 return json.load(f)
         except (FileNotFoundError, IOError):
             print("\x1b[1;31m[ERROR]:\x1b[0m settings.json not found, create a new with default data, please fill data with yours")
-            jsonSettings: Dict = {
+            jsonSettings: Dict[str, any] = {
                 "warns":{
                     "ban-warns-reach": True,
                     "max-warns": 3,
@@ -53,6 +53,6 @@ class DataController:
             
             return jsonSettings
 
-    async def save_data(self, data: dict):
+    async def save_data(self, data: dict) -> None:
         with open(self.dataFile, 'w') as f:
             json.dump(data, f, ensure_ascii=True, indent=4)

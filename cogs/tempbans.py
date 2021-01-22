76544ba5f9ede570832589c5b2cd56af6025e10e
Piotrek Markiewicz <64311472+sectasy0@users.noreply.github.com>
2021-01-22 09:23:02 +0100

    Update tempbans.py

--- cogs/tempbans.py
+++ cogs/tempbans.py
@@ -1,23 +1,25 @@
 from discord.ext import commands
 
-from discord import Member
+from discord import Member, Client
 from utils.tempbans import BansDataController
+
+from typing import Dict
 
 class TempBans(commands.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: Client = client
 
-        self.bans = BansDataController().get_bans()
+        self.bans: Dict[str, any] = BansDataController().get_bans()
     
     @commands.Cog.listener()
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         print("[*] Tempbans module loaded successfuly")
 
     @commands.command(name='tempban')
     @commands.has_permissions(ban_members=True)
-    async def tempban(self, ctx, member: Member, minutes=10):
+    async def tempban(self, ctx, member: Member, minutes=10) -> None:
         print(f"{member} {minutes}")
 
-def setup(client):
+def setup(client) -> None:
     client.add_cog(TempBans(client))
